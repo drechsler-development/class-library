7c6531b69bbeb8ba6e11497a4893dc7de536b34a
kugelschreiber1978 <info@drechsler-development.de>
2022-04-08 09:09:16 +0200

    - add Monolog as Logger

--- src/DD/Mailer/Mailer.php
+++ src/DD/Mailer/Mailer.php
@@ -5,6 +5,8 @@ namespace DD\Mailer;
 use DD\Exceptions\ValidationException;
 use DD\SystemType;
 use DD\Utils;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
 
@@ -18,17 +20,19 @@ use PHPMailer\PHPMailer\PHPMailer;
  * I am using a php file named domaincontroller.php I include on top of any script. In that file I define this SYSTEMTYPE based on the domain. Like if ($domain == "test.domain.de") define("SYSTEMTYPE,SystemType::TEST);
  * You also must declare the SMTP_USER, SMTP_PASS, SMTP_SERVER somehwere as global constants, to be able to send emails via SMTP
  */
-class Mailer extends PHPMailer {
+class Mailer extends PHPMailer
+{
 
 	const EMAIL_SUBJECT_EXCEPTION                   = "Exception";
-	const EMAIL_SUBJECT_DEBUG                       = 'DEBUG Exception';
+	const EMAIL_SUBJECT_DEBUG                       = 'DEBUG Information';
 	const EMAIL_SUBJECT_DB_EXCEPTION                = 'DB Exception';
 	const EMAIL_SUBJECT_INVALID_ARGUMNENT_EXCEPTION = 'Invalid Argument Exception';
-	const EMAIL_SUBJECT_VALIDATION_EXCEPTION 		= 'Validation Exception';
-	const LOG_FOLDER								= 'logs/email';
+	const EMAIL_SUBJECT_VALIDATION_EXCEPTION        = 'Validation Exception';
+	const EMAIL_SUBJECT_PERMISSION_EXCEPTION        = 'Permission Exception';
+	const LOG_FOLDER                                = 'logs/email';
 
 	private bool $isComment;
-	public bool $isAdminMail = false;
+	public bool  $isAdminMail = false;
 
 	public function __construct ($exceptions = null, $isComment = false) {
 
@@ -36,26 +40,48 @@ class Mailer extends PHPMailer {
 
 		$this->isSMTP (); //Set the use of mailer via "SMTP"
 
-		$this->CharSet 		= parent::CHARSET_UTF8;
-		$this->Username 	= defined ("SMTP_USER") ? SMTP_USER : '';
-		$this->Password 	= defined ("SMTP_PASS") ? SMTP_PASS : '';
-		$this->Host 		= defined ("SMTP_SERVER") ? SMTP_SERVER : '';
-		$this->isComment 	= (bool)$isComment;
-		$this->SMTPAuth 	= defined ("SMTP_AUTH") ? SMTP_AUTH : true;
-		$this->SMTPSecure 	= 'tls';
+		$this->CharSet    = parent::CHARSET_UTF8;
+		$this->Username   = defined ("SMTP_USER") ? SMTP_USER : '';
+		$this->Password   = defined ("SMTP_PASS") ? SMTP_PASS : '';
+		$this->Host       = defined ("SMTP_SERVER") ? SMTP_SERVER : '';
+		$this->isComment  = (bool)$isComment;
+		$this->SMTPAuth   = defined ("SMTP_AUTH") ? SMTP_AUTH : true;
+		$this->SMTPSecure = 'tls';
 
-		$systemType = defined("SYSTEMTYPE") ? SYSTEMTYPE : '';
+		$systemType = defined ("SYSTEMTYPE") ? SYSTEMTYPE : '';
+		$folder     = defined ("PHP_MAILER_LOGS") ? PHP_MAILER_LOGS : '';
+		$isFolder   = !empty($folder) && is_dir ($folder);
 
-		if($systemType == SystemType::DEV){
+		if($isFolder){
 
-			$this->SMTPDebug = 1;
-			$folder = defined ("PHP_MAILER_LOGS") ? PHP_MAILER_LOGS : '';
-			if(!empty($folder) && is_dir($folder)){
+			switch($systemType){
 
-				$this->Debugoutput = function($str, $level) {
-					global $folder;
-					file_put_contents ($folder.'/'.date ('Y-m-d').'.log', date ('Y-m-d H:i:s')."\t$level\t$str\n", FILE_APPEND | LOCK_EX);
-				};
+				case SystemType::DEV:
+				case SystemType::DEMO:
+				case SystemType::LOCAL:
+				case SystemType::STAGING:
+				case SystemType::REFERENCE:
+					$level = Logger::DEBUG;
+					break;
+				case SystemType::TEST:
+					$level = Logger::INFO;
+					break;
+				case SystemType::PROD:
+					$level = Logger::WARNING;
+					break;
+				default:
+					$level = Logger::ERROR;
+					break;
+			}
+
+			if ($systemType == SystemType::DEV) {
+
+				$logger = new Logger('logger');
+				$logger->pushHandler (new StreamHandler($folder.'/'.date ('Y-m-d').'.log', $level));
+
+				$this->SMTPDebug = 1;
+
+				$this->Debugoutput = $logger;
 
 			}
 
@@ -69,9 +95,9 @@ class Mailer extends PHPMailer {
 	 * @param bool $replaceInNonProdSystems
 	 * @throws Exception
 	 */
-	public function DDAddTo(string $email, string $name = "", bool $replaceInNonProdSystems = true){
+	public function DDAddTo (string $email, string $name = "", bool $replaceInNonProdSystems = true) {
 
-		$email = $this->PossibleReplaceEmail($email, $replaceInNonProdSystems);
+		$email = $this->PossibleReplaceEmail ($email, $replaceInNonProdSystems);
 		parent::addAddress ($email, $name);
 
 	}
@@ -82,9 +108,9 @@ class Mailer extends PHPMailer {
 	 * @param bool $replaceInNonProdSystems
 	 * @throws Exception
 	 */
-	public function DDAddCc(string $email, string $name = "", bool $replaceInNonProdSystems = true){
+	public function DDAddCc (string $email, string $name = "", bool $replaceInNonProdSystems = true) {
 
-		$email = $this->PossibleReplaceEmail($email, $replaceInNonProdSystems);
+		$email = $this->PossibleReplaceEmail ($email, $replaceInNonProdSystems);
 		parent::addCC ($email, $name);
 
 	}
@@ -95,43 +121,44 @@ class Mailer extends PHPMailer {
 	 * @param bool $replaceInNonProdSystems
 	 * @throws Exception
 	 */
-	public function DDAddBcc(string $email, string $name = "", bool $replaceInNonProdSystems = true){
+	public function DDAddBcc (string $email, string $name = "", bool $replaceInNonProdSystems = true) {
 
-		$email = $this->PossibleReplaceEmail($email, $replaceInNonProdSystems);
+		$email = $this->PossibleReplaceEmail ($email, $replaceInNonProdSystems);
 		parent::addBCC ($email, $name);
 
 	}
 
 	/**
-	 * @return bool|void
+	 * @param bool $sendToDeveloper
+	 * @return bool
 	 * @throws Exception
 	 */
-	public function DDSend($sendToDeveloper = false): bool {
+	public function DDSend (bool $sendToDeveloper = false): bool {
 
 		//Add developer in BCC
-		$email = defined("EMAIL_DEVELOPER") ? EMAIL_DEVELOPER : '';
-		if(!empty($email) && $sendToDeveloper){
+		$email = defined ("EMAIL_DEVELOPER") ? EMAIL_DEVELOPER : '';
+		if (!empty($email) && $sendToDeveloper) {
 			//To prevent the replacemnet in NON-PROD environments, we need to set the parameter to false!!!
-			self::DDAddBcc($email,"",false);
+			self::DDAddBcc ($email, "", false);
 		}
 
 		//Check if standard values from the phpmailer class have been used and replace it with the constants if defined
-		if($this->From == 'root@localhost' || $this->From == ''){
-			$from = defined ("SMTP_FROM") ? SMTP_FROM : '';
+		if ($this->From == 'root@localhost' || $this->From == '') {
+			$from     = defined ("SMTP_FROM") ? SMTP_FROM : '';
 			$fromName = defined ("SMTP_FROM_NAME") ? SMTP_FROM_NAME : '';
-			$this->setFrom($from, $fromName);
+			$this->setFrom ($from, $fromName);
 		}
 
 		//In case we are using the comment functionality this won't be processed
 		//as comments (CHats) will be sent to a separate mailbox where the system will handle the email in a different way
-		if (!$this->isComment){
+		if (!$this->isComment) {
 
-			$systemType = defined("SYSTEMTYPE") ? SYSTEMTYPE : '';
-			$emailDeveloper = defined("EMAIL_DEVELOPER") ? EMAIL_DEVELOPER : '';
+			$systemType     = defined ("SYSTEMTYPE") ? SYSTEMTYPE : '';
+			$emailDeveloper = defined ("EMAIL_DEVELOPER") ? EMAIL_DEVELOPER : '';
 
-			if($this->isAdminMail){
+			if ($this->isAdminMail) {
 				$email = $emailDeveloper;
-			}else{
+			} else {
 
 				switch ($systemType) {
 					case SystemType::PROD:
@@ -151,7 +178,7 @@ class Mailer extends PHPMailer {
 
 			}
 
-			if(isset($email)) {
+			if (isset($email)) {
 				self::DDAddCc ($email, "", false);
 			}
 
@@ -164,7 +191,8 @@ class Mailer extends PHPMailer {
 	/**
 	 * @param $subject
 	 */
-	public function DDSubject($subject){
+	public function DDSubject ($subject) {
+
 		$this->Subject = $this->PossibleReplaceSubject ($subject);
 	}
 
@@ -173,9 +201,9 @@ class Mailer extends PHPMailer {
 	 * @param bool $replaceInNonProdSystems
 	 * @return mixed
 	 */
-	private function PossibleReplaceEmail(string $email, bool $replaceInNonProdSystems = true): string {
+	private function PossibleReplaceEmail (string $email, bool $replaceInNonProdSystems = true): string {
 
-		if (!$this->isComment && $replaceInNonProdSystems){
+		if (!$this->isComment && $replaceInNonProdSystems) {
 
 			$systemType     = defined ('SYSTEMTYPE') ? SYSTEMTYPE : '';
 			$emailDeveloper = defined ('EMAIL_DEVELOPER') ? EMAIL_DEVELOPER : '';
@@ -206,9 +234,9 @@ class Mailer extends PHPMailer {
 	 * @param $subject
 	 * @return string
 	 */
-	private function PossibleReplaceSubject($subject): string {
+	private function PossibleReplaceSubject ($subject): string {
 
-		$systemType     = defined ('SYSTEMTYPE_NAME') ? SYSTEMTYPE_NAME : '';
+		$systemType = defined ('SYSTEMTYPE_NAME') ? SYSTEMTYPE_NAME : '';
 
 		return $systemType != SystemType::PROD ? $systemType." :: ".$subject : $subject;
 
@@ -226,8 +254,8 @@ class Mailer extends PHPMailer {
 
 		try {
 
-			$recipient = defined("EMAIL_DEVELOPER") ? EMAIL_DEVELOPER : '';
-			if(empty($recipient)){
+			$recipient = defined ("EMAIL_DEVELOPER") ? EMAIL_DEVELOPER : '';
+			if (empty($recipient)) {
 				throw new ValidationException("No constant EMAIL_DEVELOPER defined");
 			}
 
@@ -248,9 +276,9 @@ class Mailer extends PHPMailer {
 			//you can add them to the end of the body, so you know who has thrown that error
 			if (!empty($_SESSION['login'])) {
 				$userFirstName = $_SESSION['login']['firstname'] ?? '';
-				$userLastname = $_SESSION['login']['lastname'] ?? '';
-				$userId = $_SESSION['login']['id'] ?? '';
-				$body .= '<br>User: '.$userFirstName.' '.$userLastname.' ('.$userId.')';
+				$userLastname  = $_SESSION['login']['lastname'] ?? '';
+				$userId        = $_SESSION['login']['id'] ?? '';
+				$body          .= '<br>User: '.$userFirstName.' '.$userLastname.' ('.$userId.')';
 			}
 
 			$mail->msgHTML ($body);
@@ -264,7 +292,7 @@ class Mailer extends PHPMailer {
 				throw new Exception($mail->ErrorInfo);
 			}
 
-		} catch (Exception | ValidationException $e) {
+		} catch (Exception|ValidationException $e) {
 
 			self::Log ($e->getMessage ());
 
@@ -288,10 +316,10 @@ class Mailer extends PHPMailer {
 				@mkdir ($path, 0777, true);
 			}
 
-			$date      = date ('Y-m-d');
+			$date       = date ('Y-m-d');
 			$time       = date ('H:i:s');
 			$text       = '';
-			$fileName  = $path.'/'.$date.'.log';
+			$fileName   = $path.'/'.$date.'.log';
 			$backTrace  = debug_backtrace ();
 			$scriptPath = $backTrace[0]['file'];
 
@@ -303,16 +331,15 @@ class Mailer extends PHPMailer {
 			$text .= $date."\t".$time."\t".$loginId."\t".$scriptPath."\t".$message;
 
 			$filestream = @fopen ($fileName, 'a');
-			if($filestream !== false){
+			if ($filestream !== false) {
 				@fwrite ($filestream, $text."\r\n");
 				@fclose ($filestream);
-			}else{
+			} else {
 				throw new Exception("File could not be wrote to the email log folder");
 			}
 
-
-		} catch (Exception $e){
-			error_log($e->getMessage ());
+		} catch (Exception $e) {
+			error_log ($e->getMessage ());
 		}
 
 	}
